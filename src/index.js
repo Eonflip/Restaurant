16200a2043704d02eee539dad57e8ae4fc39acf2
Ian Jones <ianj1993@gmail.com>
2024-08-11 14:25:51 -0600

    Updated with constructor functions for html elements

--- src/index.js
+++ src/index.js
@@ -1,5 +1,69 @@
-import createHome from './home';
-import './styles.css';
+import createHome from './pages/home';
+import createMenu from './pages/menu';
+import './styles/home.css';
+import './styles/menu.css';
 
+createNav();
 
 createHome();
+
+const main = document.getElementById('main');
+
+function createNav() {
+    const nav = document.getElementById('nav');
+    const buttonContainer = createContainer('button-container', nav);
+    const homeButton = createButton('Home', buttonContainer);
+    const menuButton = createButton('Menu', buttonContainer);
+    const aboutButton = createButton('About', buttonContainer);
+
+    homeButton.addEventListener('click', () => {
+        nav.innerHTML = '';
+        main.innerHTML = '';
+        createNav();
+        createHome();
+    })
+
+    menuButton.addEventListener('click', () => {
+        nav.innerHTML = '';
+        main.innerHTML = '';
+        createNav();
+        createMenu();
+    })
+}
+
+
+
+
+
+function createElement({ tag, content = '', className = '', src = '', parent }) {
+    const element = document.createElement(tag);
+    if (content) {
+        element.textContent = content;
+    }
+    if (className) {
+        element.classList.add(className);
+    }
+    if (src) {
+        element.src = src;
+    }
+    if (parent) {
+        parent.appendChild(element);
+    }
+    return element;
+}
+
+function createContainer(className, parent) {
+    return createElement({ tag: 'div', className, parent });
+}
+
+function createTextElement(tag, content, parent, className = '') {
+    return createElement({ tag, content, className, parent });
+}
+
+function createImage(src, className, parent) {
+    return createElement({ tag: 'img', src, className, parent })
+}
+
+function createButton(content, parent, className = '') {
+    return createElement({ tag: 'button', content, parent, className });
+}
